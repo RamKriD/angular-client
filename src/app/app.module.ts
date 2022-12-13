f5241ef2762f678b797199d4c7dff1da378d2952
Prakrishth <ram.k.das@outlook.com>
2022-12-13 17:19:01 +0530

    Bootstrapping

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    CommentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
